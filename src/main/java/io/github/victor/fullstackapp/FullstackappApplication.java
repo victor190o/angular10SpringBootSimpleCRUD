23147d383b073ca15e6991266a25dddd155f19ed
victore <victor.eduardo.sntos@gmail.com>
2021-02-02 22:03:40 -0300

    my first commit to init repository

---
+++ src/main/java/io/github/victor/fullstackapp/FullstackappApplication.java
@@ -0,0 +1,15 @@
+package io.github.victor.fullstackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackappApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FullstackappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/victor/fullstackapp/FullstackappApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.victor.fullstackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackappApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
